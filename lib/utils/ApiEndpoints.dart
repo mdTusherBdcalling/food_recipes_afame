d294aece2a7c0a575da11ebaacf51ea6292e0f4d
mdTusherBdcalling <mdtusher.bdcalling@gmail.com>
2025-08-10 11:14:57 +0600

    https://api.mamafefood.com/ this url set

--- lib/utils/ApiEndpoints.dart
+++ lib/utils/ApiEndpoints.dart
@@ -1,8 +1,8 @@
 class ApiEndpoints {
   static const String baseUrl =
-      'http://172.252.13.74:8059/api/v1/'; // Replace with actual base URL
+      'https://api.mamafefood.com/api/v1/'; // Replace with actual base URL
   static const String baseImageUrl =
-      'http://172.252.13.74:8059'; // Replace with actual base image URL
+      'https://api.mamafefood.com/'; // Replace with actual base image URL
 
   // Auth Endpoints
   static const String register = 'auth/register';
